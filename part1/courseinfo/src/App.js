f6f2637e0721ab45f9543b25c82f18c4685c71b8
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-03-23 20:24:07 -0300

    course information step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const App = () => {
+
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -10,13 +11,38 @@ const App = () => {
   const exercises3 = 14
   
 
+
+  const Header = (props) => {
+    return (
+      <div>
+        <p>{props.course}</p>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <p>{part1} {exercises1}</p>
+        <p>{part2} {exercises2}</p>
+        <p>{part3} {exercises3}</p>
+      </div>
+    )
+  }
+
+  const Total = () => {
+    return (
+      <div>
+        <p>{exercises1 + exercises2 + exercises3 }</p>
+      </div>
+    )
+  }
+
   return (
   <div>
-    <h1>{course}</h1>
-  <p> {part1} {exercises1}</p>
-  <p> {part2} {exercises2}</p>
-  <p> {part3} {exercises3}</p>
-  <p> Number of exercises {exercises1 + exercises2 + exercises3 }</p>
+    <Header course={course} />
+    <Content />
+    <Total />
   </div>
   )
 
