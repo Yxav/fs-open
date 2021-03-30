513fe99e6cebcaad84c84d734f02ba09ac31b2ff
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-03-30 16:24:07 -0300

    step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,21 +10,42 @@ const Display = (props) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }  
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }  
+  
+  const handleBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
+
+
+
 
   return (
     <div>
       <h2>Give feedback</h2>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
       <h2>Statistics</h2>
-      <Display value={good} text="Good" />
-      <Display value={neutral} text="Neutral" />
-      <Display value={bad} text="Bad" />
+      <Display value={good} text="good" />
+      <Display value={neutral} text="neutral" />
+      <Display value={bad} text="bad" />
+      <Display value={total} text="total" />
+      <Display value={(good * 1 + neutral * 0 + bad * -1)/total} text="average" />
+      <Display value={good/total + " %"} text="positive" />
 
 
     </div>
