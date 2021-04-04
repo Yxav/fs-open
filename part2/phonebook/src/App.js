a1c35cee43670e82a755c7592cf67913befde79d
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-04-04 09:20:45 -0300

    phonebook step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ filter, setfilter ] = useState('')
+  
 
   const handleName = e => setNewName(e.target.value)
 
   const sendPerson = e => {
     e.preventDefault()
     const peoples = [...persons]
+
+    setfilter(newName)
+
+    const filtered = peoples.some(people => people.name === filter)
+    if (filtered) return alert(`${newName} is already added to phonebook`)
+
     peoples.push({name: newName})
     setPersons(peoples)
   }
@@ -28,7 +37,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <div className="results">
         {persons.map((person,id) => <p key={id}>{person.name}</p> )}
+      </div>
     </div>
   )
 }
