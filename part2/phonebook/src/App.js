1686d46356559d279e53885257d3cf93dc14cede
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-04-07 09:59:54 -0300

    phonebook:step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import Persons from './components/Persons.js'
 
+import personsServices from './services/persons.js'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setfilter] = useState('')
   const [filtered, setFiltered] = useState('')
+
+
+  useEffect(() => personsServices.getData().then(res => setPersons(res)), [])
 
 
   const handleFilter = e => {
