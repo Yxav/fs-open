5472bd61769ac31efda6eca52bdfe9aa98de482a
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-04-15 12:10:36 -0300

    blog-list part4:ex4.7

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,5 +1,45 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Nicolas Cage',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }, 
+  {
+    _id: '233ffssdfdfs',
+    title: 'Go To Google Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 20,
+    __v: 0
+  },  {
+    _id: '233ffssdfdfs',
+    title: 'Go To Google Considered Harmful',
+    author: 'Edward Diesel',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 50,
+    __v: 0
+  },  {
+    _id: '233ffssdfdfs',
+    title: 'Go To Yahoo Considered Harmful',
+    author: 'Apolo Creed',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 22,
+    __v: 0
+  },{
+    _id: '233ffssdfdfs',
+    title: 'Hot trainee Rocky Balboa',
+    author: 'Apolo Creed',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 25,
+    __v: 0
+  },
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -54,6 +94,23 @@ describe('favorite blogs', () => {
     })
   })
 
+})
+
+describe('mostBlogs', () => {
+  test('Should return author that has most blogs', () => {
+    const result = listHelper.mostBlogs(listWithBlogs)
+    expect(result).toEqual({
+      author: "Apolo Creed",
+      blogs: 2
+    })
+  })
+})
+
+describe('mostLikes', () => {
+  test('Should return author that has most likes', () => {
+    const result = listHelper.mostLikes(listWithBlogs)
+    expect(result).toEqual({ author: 'Edward Diesel', likes: 50 })
+  })
 })
 
 
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,14 +1,48 @@
+const _ = require('lodash')
+
+
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = (posts) => posts.length === 1 ? posts[0].likes : posts.map(post => post.likes)
 
-const favoriteBlog = (blogs) =>{
+const favoriteBlog = (blogs) => {
   if (!blogs || blogs.length === 0) return null
 
-  return blogs.reduce((a,b) => a.likes > b.likes ? a : b)
+  return blogs.reduce((a, b) => a.likes > b.likes ? a : b)
 
-} 
+}
 
-module.exports = { dummy, totalLikes, favoriteBlog}
+const mostBlogs = (blogList) => {
+  const mostBlog = _
+    .chain(_.map(blogList, 'author'))
+    .countBy()
+    .toPairs()
+    .maxBy(_.last)
+    .value()
+
+  return {
+    author: mostBlog[0],
+    blogs: mostBlog[1]
+  }
+}
+
+const mostLikes = (blogList) => {
+  if (blogList.length === 0) return null
+
+  const groupBlogs = _.groupBy(blogList, 'author')
+  const authorsKeyArray = Object.keys(groupBlogs)
+    .map(author => {
+      return {author, likes: totalLikes(groupBlogs[author])}
+    })
+    .sort((a,b) => b.likes - a.likes)
+
+  return {
+    author: authorsKeyArray[0].author,
+    likes: authorsKeyArray[0].likes
+  }
+
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
